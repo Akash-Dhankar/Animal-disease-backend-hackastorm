5e0913ee43ee0a173f363c6cd1ecb9512693b97f
Akash Dhankar <adunique212@gmail.com>
2025-04-09 00:02:47 +0530

    fix: bind flask app to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -23,4 +24,5 @@ def predict():
     return jsonify({"predicted_disease": prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
